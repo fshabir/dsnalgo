5cca54b186f6b09c64d93fef91dfb8f9dd2acf14
fshabir <iamfurhan@gmail.com>
2019-12-12 22:54:01 +0900

    Use strstr() and strchr() to find char/string

--- c++/strings/counter2.cpp
+++ c++/strings/counter2.cpp
@@ -17,5 +17,32 @@ int main() {
 		}
 	} while (pos != string::npos);
 	cout << needle << " found " << count << " times" << endl;
+
+	// using strstr()
+	const char * s1 = s0.data();
+	const char * needle1 = needle.data();
+	count = 0;
+
+	do {
+		s1 = strstr(s1, needle1);
+		if (s1 != NULL) {
+			s1++;
+			count += 1;
+		}
+	} while(s1 != NULL);
+	cout << needle1 << " found " << count << " times using strstr()" << endl;
+
+	s1 = s0.data();
+	count = 0;
+
+	do {
+		s1 = strchr(s1, 'i');
+		if (s1 != NULL) {
+			s1++;
+			count += 1;
+		}
+	} while(s1 != NULL);
+
+	cout << "i found " << count << " times using strchr()" << endl;
 	return 0;
 }
