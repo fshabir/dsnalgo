89eddefc3d13c5e322a582c115d2c431af8f1df2
fshabir <iamfurhan@gmail.com>
2019-12-16 21:44:01 +0900

    Add another method to pass around 2D arrays

--- c++/arrays/2d.cpp
+++ c++/arrays/2d.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void printMatrix(int **arr, int length, int width) {
+	cout << "In function: " << __func__ << " on line number: " << __LINE__ << endl;
 	cout << "Matrix: " << endl;
 	for (size_t i=0; i<length; i++) {
 		for (size_t j=0; j<width; j++) {
@@ -11,6 +12,17 @@ void printMatrix(int **arr, int length, int width) {
 		}
 		cout << endl;
 	}
+}
+
+void printMatrix(int arr[][2], int length, int width) {
+	cout << "In function: " << __func__ << " on line number: " << __LINE__ << endl;
+	cout << "Matrix: " << endl;
+	for (size_t i=0; i<length; i++) {
+		for (size_t j=0; j<width; j++) {
+			cout << arr[i][j] << "  ";
+		}
+		cout << endl;
+	}	
 }
 
 int main() {
@@ -29,5 +41,16 @@ int main() {
 	}
 
 	printMatrix(points, 5, 3);
+
+	int _points[3][2];
+
+	cout << endl;
+	for (size_t i=0; i<3; i++) {
+		for (size_t j=0; j<2; j++) {
+			cin >> _points[i][j];
+		}
+		cout << endl;
+	}
+	printMatrix(_points, 3, 2);
 	return 0;
 }
