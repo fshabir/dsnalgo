33760f7b94c0ca135f4e1b3a558a853cdba1b6f4
fshabir <iamfurhan@gmail.com>
2019-12-22 23:40:49 +0900

    Add linked list node skeleton

---
+++ c++/linked_list/node.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node * next;
+
+	Node() {
+		this->next = nullptr;
+	}
+};
+
+int main() {
+	Node * head = nullptr;
+	
+	return 0;
+}
