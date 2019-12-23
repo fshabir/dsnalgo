edeb7ad6d5f7505622387fa398ff432de6e3b9f4
fshabir <iamfurhan@gmail.com>
2019-12-23 23:55:17 +0900

    Add driver program for using LinkedList

--- c++/linked_list/linkedlist.cpp
+++ c++/linked_list/linkedlist.cpp
@@ -13,11 +13,41 @@ public:
 	LinkedList() {
 		head = nullptr;
 	}
+
+	void insert(int data) {
+		Node * node = new Node;
+		node->data = data;
+		node->next = nullptr;
+
+		if (!head) {
+			head = node;
+		} else {
+			Node * start = head;
+			while(start->next) {
+				start = start->next;
+			}
+			start->next = node;
+		}
+	}
+
+	void print() {
+		cout << "LinkedList: ";
+		Node * start = head;
+		while(start) {
+			cout << start->data << " ";
+			start = start->next;
+		}
+		cout << endl;
+	}
+
 };
 
 int main() {
 
 	LinkedList list;
-
+	list.insert(3);
+	list.insert(5);
+	list.insert(7);
+	list.print();
 	return 0;
 }
