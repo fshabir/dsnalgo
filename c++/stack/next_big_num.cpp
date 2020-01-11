d787b4eb2fc50ca289decac1919b76531b7e8aea
fshabir <iamfurhan@gmail.com>
2020-01-11 13:53:24 +0900

    Attempt to use queue for next_big_number problem

--- c++/stack/next_big_num.cpp
+++ c++/stack/next_big_num.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -25,18 +26,44 @@ int* nextGreaterElement(int *arr, int size) {
 	return result;
 }
 
+int* nextGreaterElement2(int *arr, int size) {
+	int * result = new int[size];
+	queue<int> q;
+	for (int i=0; i<size-1; i++) {
+		if (arr[i] < arr[i+1]) {
+			q.push(arr[i+1]);
+		}
+	}
+
+	int cnt = 0;
+	while (!q.empty()) {
+		int t = q.front();
+		while (arr[cnt] != t) {
+			result[cnt++] = t;
+		}
+		q.pop();
+	}
+
+	while (cnt < size) {
+		result[cnt++] = -1;
+	}
+	return result;
+}
+
 
 int main () {
 
+	// 13,3,12,16,15,1
 	int * arr = new int[6];
-	arr[0] = 4;
-	arr[1] = 6;
-	arr[2] = 3;
-	arr[3] = 2;
-	arr[4] = 8;
+	arr[0] = 13;
+	arr[1] = 3;
+	arr[2] = 12;
+	arr[3] = 16;
+	arr[4] = 15;
 	arr[5] = 1;
 
-	arr = nextGreaterElement(arr, 6);
+	//arr = nextGreaterElement(arr, 6);
+	arr = nextGreaterElement2(arr, 6);
 
 	cout << "[ ";
 	for (int i=0; i<6; i++) {
